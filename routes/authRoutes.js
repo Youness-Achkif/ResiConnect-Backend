a026b652648849f8c79fe680a3c6ea761ef71281
Youness Achkif <youness.achkif@gmail.com>
2026-05-21 15:39:16 +0200

    feat: route GET /api/auth/gestionnaire publique

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, supprimerUser } = require('../controllers/authController');
+const { register, login, supprimerUser, getGestionnaire } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.post('/register', authMiddleware, register);
 router.post('/login', login);
+router.get('/gestionnaire', getGestionnaire);
 router.delete('/users/:id', authMiddleware, supprimerUser);
 
 module.exports = router;

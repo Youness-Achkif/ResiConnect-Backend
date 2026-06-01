6f374e9bbee8dd10fbbe9405acab3483e7955cd7
Youness Achkif <youness.achkif@gmail.com>
2026-06-01 11:55:55 +0200

    feat(messages): add DELETE conversation route for gestionnaire

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
-const { getMessages, envoyerMessage, marquerLu } = require('../controllers/messageController');
+const { getMessages, envoyerMessage, marquerLu, supprimerConversation } = require('../controllers/messageController');
 
 router.use(authMiddleware);
 
 router.get('/', getMessages);
 router.post('/', envoyerMessage);
 router.put('/:id/lu', marquerLu);
+router.delete('/conversation/:userId', supprimerConversation);
 
 module.exports = router;
